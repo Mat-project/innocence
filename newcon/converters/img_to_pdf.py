275a0e4c53f7074d6730113a3a498024e6a28b59
MADHESH K <madheshk7239087@gmail.com>
2025-04-03 21:32:35 +0530

    del file converter

--- newcon/converters/img_to_pdf.py
+++
@@ -1,9 +0,0 @@
-from fpdf import FPDF
-from PIL import Image
-
-def convert_img_to_pdf(input_path, output_path):
-    image = Image.open(input_path)
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.image(input_path, 0, 0, 210, 297)  # A4 Size
-    pdf.output(output_path)
